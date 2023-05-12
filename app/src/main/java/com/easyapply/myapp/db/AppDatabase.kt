d97078144fb54f3cef9805133bc777601acb02e9
Ahmed-Samirr <ahmedsamir2382@gmail.com>
2023-05-12 18:06:29 +0300

    fixed database error

--- app/src/main/java/com/easyapply/myapp/db/AppDatabase.kt
+++ app/src/main/java/com/easyapply/myapp/db/AppDatabase.kt
@@ -2,9 +2,9 @@ package com.easyapply.myapp.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.easyapply.myapp.domain.model.Post
+import com.easyapply.myapp.data.remote.dto.PostDto
 
-@Database(entities = [Post::class], version = 1)
+@Database(entities = [PostDto::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun postDao(): PostDao
 }
